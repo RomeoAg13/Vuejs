e8a2f77375b2fe6d290d358e42e8db5c84e96a16
Romeo <romeoago004@gmail.com>
2024-03-08 09:02:22 +0100

    d

--- my-vue-app/src/utils/cart.js
+++ my-vue-app/src/utils/cart.js
@@ -23,8 +23,14 @@ const cartPrice = () => {
 
 // if recupere les donnees du panier depuis le localStorage, deja present dans chaque const
 
-if (localStorage.getItem('cartState')) {
-    state.value.cart = JSON.parse(localStorage.getItem('cartState'));
+if (localStorage.getItem('cartState')) {  // recupere les donnees du panier 
+    const cartState = localStorage.getItem('cartState'); // si donnees existent alors associe a cartState
+    try {
+        state.value.cart = JSON.parse(cartState);  //ensuite le code try de convertir en objet 
+    } catch (error) {
+        console.error('Error parsing cart state from localStorage:', error);
+        // Handle the error appropriately, possibly by resetting the cart
+    }
 }
 
 // const qui permet d'ajouter dans le panier
@@ -32,10 +38,10 @@ const addToCart = (product) => {
     const existingProductIndex = state.value.cart.findIndex(cartProd => cartProd.product.id === product.id);
 
     if (existingProductIndex !== -1) {
-        // Si le produit existe déjà dans le panier, incrémentez juste la quantité
+        // Si le produit existe déjà dans le panier, +1
         state.value.cart[existingProductIndex].quantity++;
     } else {
-        // Sinon, ajoutez un nouvel élément au panier
+        // Sinon,ajouter au panier
         state.value.cart.push({ product, quantity: 1 });
     }
 
